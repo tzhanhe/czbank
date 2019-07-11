189ec38aa9e1040c99829fa0be8d6fed73e3b277
Ryanboulay <ryanboulay@foxmail.com>
2019-07-11 09:46:48 +0800

    项目基本架构

---
+++ youthloan_springboot/src/main/java/com/czbank/youthloan_springboot/YouthloanSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.czbank.youthloan_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthloanSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthloanSpringbootApplication.class, args);
+    }
+
+}
